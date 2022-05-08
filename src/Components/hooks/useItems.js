afe3b8675da2e3232df67eda487b3456aed7a33c
Jobair Shakib <shakib.jobair@gmail.com>
2022-05-08 22:24:58 +0600

    manage inventories fixed

--- src/Components/hooks/useItems.js
+++ src/Components/hooks/useItems.js
@@ -4,7 +4,7 @@ const useItems = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch(`https://murmuring-temple-05921.herokuapp.com/item`)
+        fetch(`https://murmuring-temple-05921.herokuapp.com/items`)
             .then(res => res.json())
             .then(data => setItems(data))
     },[]);
